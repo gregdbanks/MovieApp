ecd299254a4d94cea246d1d5d4e48ab20adfc6fb
Greg Banks <gregbanks@Gregs-MacBook-Pro.local>
2021-10-23 07:14:01 -0500

    Added Seed Data if no movies exist

--- Data/MvcMovieContext.cs
+++ Data/MvcMovieContext.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using MvcMovie.Models;
 
-    public class MvcMovieContext : DbContext
+public class MvcMovieContext : DbContext
     {
         public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
             : base(options)
